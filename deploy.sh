5e38a9725a0592bb51ead30d2a931e23c3fe62e6
ianfebi01 <ianfebi01@gmail.com>
2023-08-31 20:51:49 +0700

    add build script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+SSH_USER=ubuntu
+SSH_HOST=51.79.222.185
+
+
+SERVER_FILE_LOCATION=app/gendut-grosir/gendut-grosir
+
+ssh -C $SSH_USER@$SSH_HOST "cd $SERVER_FILE_LOCATION && \
+  git checkout development && \
+  git pull && \
+  docker-compose down && \
+  docker-compose up -d --build && \
+  exit"
